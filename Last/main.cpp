69f9be49b305a8cc033dcae68919d43a00f4f515
Santiago Sanabria <german.sanabria@ucr.ac.cr>
2022-05-14 12:52:38 -0600

    Initial Commit

---
+++ Last/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Last
+//
+//  Created by Santiago Sanabria Serrano on 5/14/22.
+//  Copyright © 2022 Santiago Sanabria. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
